3f639397d2a53c4968a137d964547ea3fdf9eed2
yizhang19801001 <yizhang19801001@gmail.com>
2019-01-25 17:46:47 +1100

    css

--- src/components/ProductOrderCard.jsx
+++ src/components/ProductOrderCard.jsx
@@ -5,12 +5,15 @@ import "../css/ProductOrderCard.css";
 const ProductOrderCard = ({ product }) => {
   return (
     <div className="component-product-order-card">
-      <div className="component-product-order-card__image-container">
-        <img src={product.image} alt="" />
+      <div className="component-product-order-card__info">
+        <div className="component-product-order-card__image-container">
+          <img src={product.image} alt="" />
+        </div>
+        <div className="component-product-order-card__name">{product.name}</div>
       </div>
-      <div className="component-product-order-card__name">{product.name}</div>
-      <div className="component-product-order-card__quantity">
-        <span className="component-product-order-card__quantity__text">
+      <div className="component-product-order-card__process-bar">
+        <span className="component-product-order-card__bar" />
+        <span className="component-product-order-card__quantity">
           {product.quantity}/{product.stock_status_id}
         </span>
       </div>
