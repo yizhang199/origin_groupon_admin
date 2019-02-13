1b82009062de65ffe2b1ee4b1c94e9ae4f4c5b39
yizhang19801001 <yizhang19801001@gmail.com>
2019-02-13 16:46:06 +1100

    report dashboard

--- src/components/ChartManageMainWindow.jsx
+++ src/components/ChartManageMainWindow.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Bar, Line, Pie } from "react-chartjs-2";
 
 import { fetchReportsSummary } from "../actions";
 import { makeDate } from "../helpers";
-import SalesByPaymentMethodChart from "./SalesByPaymentMethodChart";
+
+import "../css/ChartManageMainWindow.css";
 
 class ChartManageMainWindow extends React.Component {
   componentDidMount() {
@@ -11,31 +13,69 @@ class ChartManageMainWindow extends React.Component {
   }
 
   renderSalesByStore = () => {
-    const data = this.props.reportSummary.sales_by_store;
-    if (!data) {
+    const orignalData = this.props.reportSummary.sales_by_store;
+    if (!orignalData) {
       return null;
     }
-    return (
-      <table>
-        <tbody>
-          {data.map(element => {
-            return (
-              <tr key={`store${element.store_id}`}>
-                <td>{element.total}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    );
+
+    let labels = [];
+    let dataArray = [];
+    orignalData.map(ele => {
+      labels = [...labels, ele.store_name];
+      dataArray = [...dataArray, ele.total];
+    });
+
+    const data = {
+      labels,
+      datasets: [
+        {
+          data: dataArray,
+          backgroundColor: ["#ffba2d", "#ff3939", "#9CD919"]
+        }
+      ]
+    };
+
+    const options = {
+      title: {
+        display: true,
+        text: "店面销量报表",
+        fontSize: 20
+      },
+      legend: {
+        position: "right",
+        labels: {
+          fontColor: "#a5a5a5"
+        }
+      }
+    };
+
+    return <Pie width={450} height={225} data={data} options={options} />;
   };
 
   renderSalesByPayment = () => {
-    const data = this.props.reportSummary.sales_by_payment;
-    if (!data) {
+    if (!this.props.reportSummary.sales_by_payment) {
       return null;
     }
-    return <SalesByPaymentMethodChart data={data} />;
+    let datasetsArray = [];
+
+    let labels = [];
+    this.props.reportSummary.sales_by_payment.map(ele => {
+      const label = ele.payment_method;
+      let dataArray = [];
+      ele.data.map(element => {
+        labels = [...labels, makeDate(element.date)];
+        dataArray = [...dataArray, element.total];
+      });
+
+      datasetsArray = [
+        ...datasetsArray,
+        { label, data: dataArray, fill: false }
+      ];
+    });
+
+    const data = { labels, datasets: datasetsArray };
+
+    return <Line data={data} width={600} height={300} />;
   };
   renderSalesByCustomer = () => {
     const data = this.props.reportSummary.sales_by_custmer;
@@ -59,24 +99,40 @@ class ChartManageMainWindow extends React.Component {
   };
 
   renderSalesByProduct = () => {
-    const data = this.props.reportSummary.sales_by_product;
-    if (!data) {
+    const orignalData = this.props.reportSummary.sales_by_product;
+    if (!orignalData) {
       return null;
     }
-    return (
-      <table>
-        <tbody>
-          {data.map(element => {
-            return (
-              <tr key={`store${element.product}`}>
-                <td>{element.product}</td>
-                <td>{element.total}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    );
+
+    let labels = [];
+    const label = "销量";
+    let dataArray = [];
+    orignalData.map(ele => {
+      labels = [...labels, ele.product_name];
+      dataArray = [...dataArray, ele.total];
+    });
+
+    const data = {
+      labels,
+      datasets: [
+        {
+          label,
+          data: dataArray,
+          backgroundColor: "#ffba2d",
+          hoverBorderWidth: 2,
+          hoverBorderColor: "#a5a5a5"
+        }
+      ],
+      options: {
+        title: {
+          display: true,
+          text: "产品销量报表",
+          fontSize: 20
+        }
+      }
+    };
+
+    return <Bar height={300} width={300} data={data} />;
   };
 
   renderSalesByDate = () => {
@@ -86,6 +142,12 @@ class ChartManageMainWindow extends React.Component {
     }
     return (
       <table>
+        <thead>
+          <tr>
+            <td>日期</td>
+            <td>销售额</td>
+          </tr>
+        </thead>
         <tbody>
           {data.map(element => {
             return (
@@ -103,12 +165,25 @@ class ChartManageMainWindow extends React.Component {
     return (
       <div className="component-chart-manage-main-window">
         <div className="component-chart-manage-main-window__dashboard">
-          {this.props.reportSummary.sales}
-          {this.renderSalesByStore()}
-          {this.renderSalesByPayment()}
-          {this.renderSalesByCustomer()}
-          {this.renderSalesByProduct()}
-          {this.renderSalesByDate()}
+          <div className="component-chart-manage-main-window__dashboard__sales">
+            总销售额：{this.props.reportSummary.sales}
+          </div>
+
+          <div className="component-chart-manage-main-window__dashboard__sales-by-payment">
+            {this.renderSalesByPayment()}
+          </div>
+          <div className="component-chart-manage-main-window__dashboard__sales-by-customer">
+            {this.renderSalesByCustomer()}
+          </div>
+          <div className="component-chart-manage-main-window__dashboard__sales-by-product">
+            {this.renderSalesByProduct()}
+          </div>
+          <div className="component-chart-manage-main-window__dashboard__sales-by-date">
+            {this.renderSalesByDate()}
+          </div>
+          <div className="component-chart-manage-main-window__dashboard__sales-by-store">
+            {this.renderSalesByStore()}
+          </div>
         </div>
       </div>
     );
