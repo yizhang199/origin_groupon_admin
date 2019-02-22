e184fc931781825b5dbfb4dd4b461ae05edca1e4
yizhang19801001 <yizhang19801001@gmail.com>
2019-02-22 17:36:06 +1100

    product order style change

--- src/components/ProductOrderCard.jsx
+++ src/components/ProductOrderCard.jsx
@@ -8,19 +8,25 @@ const ProductOrderCard = ({ product }) => {
     width: `${(productRemaining * 100) / product.stock_status_id}%`
   };
   const containerStyle = {
-    backgroundImage: `url(${product.image})`
+    // backgroundImage: `url(${product.image})`
+    backgroundColor: "#fff"
   };
   return (
     <div className="component-product-order-card" style={containerStyle}>
-      <div className="component-product-order-card__name">{product.name}</div>
-
-      <div className="component-product-order-card__process-bar">
-        <span className="component-product-order-card__bar" style={cardStyle} />
-        <span className="component-product-order-card__quantity-container">
-          <span className="component-product-order-card__quantity">
-            {productRemaining}/{product.stock_status_id}
+      <div className="component-product-order-card__content">
+        <div className="component-product-order-card__name">{product.name}</div>
+        <span className="component-product-order-card__quantity">
+          <span className="component-product-order-card__quantity__sold">
+            {productRemaining}
+          </span>
+          /
+          <span className="component-product-order-card__quantity__max">
+            {product.stock_status_id}
           </span>
         </span>
+      </div>
+      <div className="component-product-order-card__process-bar">
+        <span className="component-product-order-card__bar" style={cardStyle} />
       </div>
     </div>
   );
---
+++ src/components/ProductOrderList_old.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import { getProducts } from "../actions";
+
+import "../css/ProductOrderList.css";
+class ProductOrderList extends React.Component {
+  componentDidMount() {
+    this.props.getProducts();
+  }
+
+  getStyle = (num, maxNum, index) => {
+    console.log(index);
+
+    return {
+      height: "45px",
+      width: "100%",
+      backgroundImage: `linear-gradient(90deg, #${
+        index % 2 === 0 ? "f55747" : "ffba2b4d"
+      } ${((maxNum - num) / maxNum) * 100}%,#fff, #fff)`
+    };
+  };
+
+  renderProducts = () => {
+    let index = 0;
+    return this.props.products.map(productGroup => {
+      return productGroup.products.map(product => {
+        index++;
+        return (
+          <tr
+            className="product-order-list__table__tbody__tr"
+            style={this.getStyle(
+              product.quantity,
+              product.stock_status_id,
+              index
+            )}
+            key={`orderProduct${product.product_id}`}
+          >
+            <td className="product-order-list__table__tbody__name">
+              {product.name}
+            </td>
+            <td className="product-order-list__table__tbody__quantity">
+              {product.quantity}
+            </td>
+            <td className="product-order-list__table__tbody__quantity">
+              {product.stock_status_id}
+            </td>
+          </tr>
+        );
+      });
+    });
+  };
+
+  render() {
+    return (
+      <div className="component-product-order-list">
+        <table className="component-product-order-list__table">
+          <tbody className="product-order-list__table__tbody">
+            {this.renderProducts()}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ products }) => {
+  return { products };
+};
+
+export default connect(
+  mapStateToProps,
+  { getProducts }
+)(ProductOrderList);
