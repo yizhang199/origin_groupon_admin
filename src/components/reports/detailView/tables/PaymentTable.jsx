9557fb78edbcb9c487fd8e47931d3b53c54f953d
yizhang19801001 <yizhang19801001@gmail.com>
2019-02-26 17:53:01 +1100

    payment compnent

--- src/components/reports/detailView/tables/PaymentTable.jsx
+++ src/components/reports/detailView/tables/PaymentTable.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const Payment = ({ reportDetails }) => {
-  return (
-    <div className="component-detail-view-table">
-      {JSON.stringify(reportDetails[0])}
-    </div>
-  );
-};
+class Payment extends React.Component {
+  render() {
+    return (
+      <div className="component-detail-view-table">
+        {JSON.stringify(this.props.reportDetails[0])}
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = ({ reportDetails }) => {
   return { reportDetails };
--- src/components/reports/detailView/tables/ProductTable.jsx
+++ src/components/reports/detailView/tables/ProductTable.jsx
@@ -1,9 +1,88 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const Product = ({ reportDetails }) => {
-  return <div>{JSON.stringify(reportDetails[0])}</div>;
-};
+class Product extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { product_name: 0, quantity: 0, total: 0 };
+  }
+  sortByCategoryName = () => {
+    const sortOrder = this.getSortOrder("product_name");
+    this.props.sortReportDetails(
+      "product_name",
+      sortOrder,
+      this.props.reportDetails
+    );
+  };
+  sortByTotal = () => {
+    const sortOrder = this.getSortOrder("total");
+    this.props.sortReportDetails("total", sortOrder, this.props.reportDetails);
+  };
+  sortByQuantity = () => {
+    const sortOrder = this.getSortOrder("quantity");
+    this.props.sortReportDetails(
+      "quantity",
+      sortOrder,
+      this.props.reportDetails
+    );
+  };
+  getSortOrder = value => {
+    let sortOrder = 0;
+    if (this.state[value] === 1) {
+      this.setState({ [value]: -1 });
+      sortOrder = -1;
+    } else {
+      this.setState({ [value]: 1 });
+      sortOrder = 1;
+    }
+    return sortOrder;
+  };
+  getIcon = value => {
+    switch (this.state[value]) {
+      case 0:
+        return "remove";
+      case 1:
+        return "arrow_upward";
+      case -1:
+        return "arrow_downward";
+      default:
+        return null;
+    }
+  };
+  renderThead = () => {
+    return (
+      <thead>
+        <tr>
+          <th onClick={this.sortByCategoryName} className="text">
+            <span>分类名称</span>
+          </th>
+          <th onClick={this.sortByTotal} className="number">
+            <span>销售额</span>
+          </th>
+          <th onClick={this.sortByQuantity} className="number">
+            <span>销售数量</span>
+          </th>
+        </tr>
+      </thead>
+    );
+  };
+  renderTbody = () => {
+    return (
+      <tbody>
+        {this.props.reportDetails.map((element, index) => {
+          const { category_name, total, quantity } = element;
+          return (
+            <tr key={`orderByCategoryRow${index}`} style={getStyle()}>
+              <td className="text">{category_name}</td>
+              <td className="number">{total}</td>
+              <td className="number">{quantity}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    );
+  };
+}
 
 const mapStateToProps = ({ reportDetails }) => {
   return { reportDetails };
