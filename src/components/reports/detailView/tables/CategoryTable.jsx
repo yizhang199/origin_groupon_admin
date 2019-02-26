9143411549eb8cd0bb6913248004be7f787aaff6
yizhang19801001 <yizhang19801001@gmail.com>
2019-02-26 17:38:56 +1100

    categoryTable componet

--- src/components/reports/detailView/tables/CategoryTable.jsx
+++ src/components/reports/detailView/tables/CategoryTable.jsx
@@ -1,32 +1,79 @@
 import React from "react";
 import { connect } from "react-redux";
 
+import { sortReportDetails } from "../../../../actions";
+import { getStyle } from "./helper";
 import "./style.css";
 
-import { getStyle } from "./helper.js";
-
-const Category = ({ reportDetails }) => {
-  const renderThead = () => {
+class Category extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { category_name: 0, quantity: 0, total: 0 };
+  }
+  sortByCategoryName = () => {
+    const sortOrder = this.getSortOrder("category_name");
+    this.props.sortReportDetails(
+      "category_name",
+      sortOrder,
+      this.props.reportDetails
+    );
+  };
+  sortByTotal = () => {
+    const sortOrder = this.getSortOrder("total");
+    this.props.sortReportDetails("total", sortOrder, this.props.reportDetails);
+  };
+  sortByQuantity = () => {
+    const sortOrder = this.getSortOrder("quantity");
+    this.props.sortReportDetails(
+      "quantity",
+      sortOrder,
+      this.props.reportDetails
+    );
+  };
+  getSortOrder = value => {
+    let sortOrder = 0;
+    if (this.state[value] === 1) {
+      this.setState({ [value]: -1 });
+      sortOrder = -1;
+    } else {
+      this.setState({ [value]: 1 });
+      sortOrder = 1;
+    }
+    return sortOrder;
+  };
+  getIcon = value => {
+    switch (this.state[value]) {
+      case 0:
+        return "remove";
+      case 1:
+        return "arrow_upward";
+      case -1:
+        return "arrow_downward";
+      default:
+        return null;
+    }
+  };
+  renderThead = () => {
     return (
       <thead>
         <tr>
-          <th className="text">
+          <th onClick={this.sortByCategoryName} className="text">
             <span>分类名称</span>
           </th>
-          <th className="number">
+          <th onClick={this.sortByTotal} className="number">
             <span>销售额</span>
           </th>
-          <th className="number">
+          <th onClick={this.sortByQuantity} className="number">
             <span>销售数量</span>
           </th>
         </tr>
       </thead>
     );
   };
-  const renderTbody = () => {
+  renderTbody = () => {
     return (
       <tbody>
-        {reportDetails.map((element, index) => {
+        {this.props.reportDetails.map((element, index) => {
           const { category_name, total, quantity } = element;
           return (
             <tr key={`orderByCategoryRow${index}`} style={getStyle()}>
@@ -39,19 +86,24 @@ const Category = ({ reportDetails }) => {
       </tbody>
     );
   };
-  return (
-    <div className="component-detail-view-table">
-      <table>
-        {renderThead()}
+  render() {
+    return (
+      <div className="component-detail-view-table">
+        <table>
+          {this.renderThead()}
 
-        {renderTbody()}
-      </table>
-    </div>
-  );
-};
+          {this.renderTbody()}
+        </table>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = ({ reportDetails }) => {
   return { reportDetails };
 };
 
-export default connect(mapStateToProps)(Category);
+export default connect(
+  mapStateToProps,
+  { sortReportDetails }
+)(Category);
